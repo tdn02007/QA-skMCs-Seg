04248228d048b9c77d24a8ab4c5bb2970f407e5a
Seonghwan Park <tdn02007@dgist.ac.kr>
2024-05-27 14:17:54 +0900

    uploaded

--- calc_metrics.py
+++ calc_metrics.py
@@ -3,8 +3,8 @@ import numpy as np
 from PIL import Image
 from skimage.filters import gaussian
 
-data_folder = "../unet/paper_result/threshold results/Yen/"
-label_folder = "../unet/paper_result/cls3_label2/"
+data_folder = "./results/"
+label_folder = "./labels/"
 
 data_list = sorted(os.listdir(data_folder))
 label_list = sorted(os.listdir(label_folder))
@@ -13,22 +13,17 @@ iou_value = 0
 dice_value = 0
 
 def calculate_iou(image1, image2):
-    """두 이미지의 IoU를 계산합니다."""
     intersection = np.logical_and(image1, image2).sum()
     union = np.logical_or(image1, image2).sum()
     iou = intersection / union
     return iou
 
 def calculate_dice(image1, image2):
-    """두 이미지의 Dice 계수를 계산합니다."""
     intersection = np.logical_and(image1, image2).sum()
     dice = (2 * intersection + 1) / (image1.sum() + image2.sum() + 1)
     return dice
 
 for i, data in enumerate(data_list):
-    if i == 40:
-        break
-
     image1 = np.array(Image.open(data_folder + data).convert("L"))
     image1 = gaussian(image1, sigma=1) > 0.5
 
@@ -42,5 +37,5 @@ for i, data in enumerate(data_list):
     dice_value += dice
 
 # print(data)
-print(f"IoU: {iou_value / 40}")
-print(f"Dice Coefficient: {dice_value / 40}")
+print(f"IoU: {iou_value / len(data_list)}")
+print(f"Dice Coefficient: {dice_value / len(data_list)}")
